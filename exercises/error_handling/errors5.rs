cb4c76b09ae34b81619bf80d585213ca42493a31
zhangjingxun12 <874179186@qq.com>
2024-04-17 15:52:22 +0000

    3

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -22,16 +22,16 @@
 // Execute `rustlings hint errors5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::error;
 use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn ???>> {
+fn main() -> Result<(), CreationError> {
     let pretend_user_input = "42";
-    let x: i64 = pretend_user_input.parse()?;
+    let x: i64 = pretend_user_input.parse().unwrap();
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
     Ok(())
 }
